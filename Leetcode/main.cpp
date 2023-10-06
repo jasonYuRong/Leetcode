8cf1fe69214765c9048c1057f15b7b460a43d0c9
jasonYuRong <jason122130@gmail.com>
2023-10-06 01:07:52 -0400

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by Jason Yeh on 10/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
